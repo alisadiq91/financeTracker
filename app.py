7a26a80ca2be6dfd70ee1f142cffd00ac1773de7
alisadiq91 <ali.sadiq91@gmail.com>
2021-05-21 17:36:23 +0000

    added bootstrap, font-awesome, jquery and static files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes = recipes)
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
