1ffb6079eb287b03c05528c9168308139ecdc377
alisadiq91 <ali.sadiq91@gmail.com>
2021-05-26 13:05:43 +0000

    created add_recipe template with form and js

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$('.add-ingredient-list-item').click(function (event) {
+    let ingredientItem = `<li class="list-item">
+                                <div class="input-field">
+                                    <i class="fas fa-times mr-4 remove-list-item"></i>
+                                    <input name="ingredients1" id="ingredients1" type="text" maxlength="100" placeholder="Add ingredient..." required>
+                                </div>
+                          </li>`;      
+                            
+    $(this).parent().before(ingredientItem);
+});
+
+// Remove ingredient list item on click
+$('#ingredients').on("click", ".remove-list-item", function (event) {
+    $(this).parent().remove();
+});
+
+$('.add-method-list-item').click(function (event) {
+    let methodItem = `<li class="list-item">
+                                <div class="input-field">
+                                    <i class="fas fa-times mr-4 remove-list-item"></i>
+                                    <input name="method1" id="ingredients1" type="text" maxlength="100" placeholder="Add method step..." required>
+                                </div>
+                          </li>`;      
+                            
+    $(this).parent().before(methodItem);
+});
+
+// Remove ingredient list item on click
+$('#method').on("click", ".remove-list-item", function (event) {
+    $(this).parent().remove();
+});
